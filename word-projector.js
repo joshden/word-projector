e69e6cc2c5e3fa60efd0b181596b6f043cff5cd2
Josh Hayden <joshden@users.noreply.github.com>
2017-09-09 19:20:45 -0500

    Scale presenter font size per presentation width
    The presenter window font size, margins, padding, etc. need to be
    dynamically scaled based on the ratio of presenter screen width
    to presentation screen width.

--- word-projector.js
+++ word-projector.js
@@ -1,6 +1,16 @@
 $(function() {
     let popup = null;
 
+    function syncPresenterFontSize() {
+        const popupWidth = popup.document.documentElement.clientWidth;
+        //const popupHeight = popup.document.documentElement.clientHeight;
+        const presenterWidth = document.documentElement.clientWidth;
+        //const presenterHeight = document.documentElement.clientHeight;
+        const presenterPercentage = String(presenterWidth / popupWidth * 100) + '%';
+        $('#presenter').css('font-size', presenterPercentage);
+        //console.log(presenterPercentage);
+    }
+
     $('#launchPresentation').click(function() {
         if (popup) {
             popup.close();
@@ -12,6 +22,8 @@ $(function() {
             popup.onload = function() {
                 $('#presenter > article').clone().appendTo(popup.document.body);
             };
+            $(popup).resize(syncPresenterFontSize);
+            $(window).resize(syncPresenterFontSize);
             $(this).text('Close Presentation');
         }
     });
