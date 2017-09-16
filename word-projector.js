9e7151915a26e08ac3127ac53089e0fe2a7cf16d
Josh Hayden <joshden@users.noreply.github.com>
2017-09-16 13:11:04 -0500

    Better handle when popup presentation is closed
    Detect closing of the popup presentation window
    
    When the user closes the popup:
    -Unselect the active song
    -Don't allow songs to be selected
    -Hide the live frame
    -Continue scaling presenter font size based on last size of popup
    -Add checks to remove JavaScript errors of popup not being defined
     and errors accessing the window

--- word-projector.js
+++ word-projector.js
@@ -1,7 +1,10 @@
 $(function() {
     let popup = null;
     let $currentSelection = null;
-    const $liveFrame = $('#liveFrame').hide();
+    const $launchPresentation = $('#launchPresentation');
+    const $liveFrame = $('#liveFrame');
+    let popupWidth = 0;
+    let popupHeight = 0;
 
     function borderWidth($obj, side) {
         return Number($obj.css(`border${side}Width`).slice(0, -2));
@@ -13,35 +16,52 @@ $(function() {
         }
     }
 
-    function syncPresenterFontSize() {
-        const popupWidth = popup.document.documentElement.clientWidth;
-        const presenterWidth = document.documentElement.clientWidth;
+    function getActiveArticle() {
+        return $('article.active');
+    }
 
+    function unselectSong() {
+        getActiveArticle().removeClass('active');
+        if (popup) {
+            popup.$('article.active').removeClass('active');
+        }
+    }
+
+    function syncPresenterFontSize() {
+        if (popup) {
+            popupWidth = popup.document.documentElement.clientWidth;
+            popupHeight = popup.document.documentElement.clientHeight;
+        }
+        const presenterWidth = document.documentElement.clientWidth;
         const presenterPercentage = String(presenterWidth / popupWidth * 100) + '%';
         $('#presenter').css('font-size', presenterPercentage);
 
-        const popupHeight = popup.document.documentElement.clientHeight;
         const liveFrameHeight = presenterWidth / popupWidth * popupHeight;
         $liveFrame.css('height', String(liveFrameHeight - borderWidth($liveFrame, 'Top') - borderWidth($liveFrame, 'Bottom')) + 'px');
         $liveFrame.css('width', String(presenterWidth - borderWidth($liveFrame, 'Left') - borderWidth($liveFrame, 'Left')) + 'px');
         resetLiveFramePosition();
     }
 
-    $('#launchPresentation').click(function() {
+    $launchPresentation.click(function() {
         if (popup) {
             popup.close();
-            popup = null;
-            $(this).text('Launch Presentation');
         }
         else {
             popup = window.open('presentation.html', '_blank', 'height=300,width=700,scrollbars=no');
             popup.onload = function() {
                 $('#presenter > article').clone().appendTo(popup.document.body);
+                popup.onunload = function() {
+                    $launchPresentation.text('Launch Presentation');
+                    $currentSelection = null;
+                    $liveFrame.hide();
+                    unselectSong();
+                    popup = null;
+                };
             };
             $(popup).resize(syncPresenterFontSize);
             $(window).resize(syncPresenterFontSize);
             syncPresenterFontSize();
-            $(this).text('Close Presentation');
+            $launchPresentation.text('Close Presentation');
         }
     });
 
@@ -63,37 +83,36 @@ $(function() {
         $('#presenter').html(wordsHtml);
 
         $('#presenter article header').add('#presenter article li').click(function() {
-            $currentSelection = $(this);
-            const $article = $currentSelection.parents('article').first();
-            const articleSelector = `article:nth-of-type(${$article.prevAll('article').length + 1})`;
-            const $activeArticle = $('article.active');
+            if (popup) {
+                $currentSelection = $(this);
+                const $article = $currentSelection.parents('article').first();
+                const articleSelector = `article:nth-of-type(${$article.prevAll('article').length + 1})`;
 
-            const isSwitchingArticle = $(articleSelector).get(0) !== $activeArticle.get(0);
-            const scrollToSelector = articleSelector + (
-                $currentSelection.is('header') ? '' 
-                : `> ol:nth-of-type(${$currentSelection.parent().prevAll('ol').length + 1}) > li:nth-of-type(${$currentSelection.prevAll('li').length + 1})`);
-            
-            const $popupHtmlBody = popup.$('html, body');
-            const popupScrollTop = popup.$(scrollToSelector).offset().top;
+                const isSwitchingArticle = $(articleSelector).get(0) !== getActiveArticle().get(0);
+                const scrollToSelector = articleSelector + (
+                    $currentSelection.is('header') ? '' 
+                    : `> ol:nth-of-type(${$currentSelection.parent().prevAll('ol').length + 1}) > li:nth-of-type(${$currentSelection.prevAll('li').length + 1})`);
+                
+                const $popupHtmlBody = popup.$('html, body');
+                const popupScrollTop = popup.$(scrollToSelector).offset().top;
 
-            if (isSwitchingArticle) {
-                $activeArticle.removeClass('active');
-                popup.$('article.active').removeClass('active');
+                if (isSwitchingArticle) {
+                    unselectSong();
+                    $popupHtmlBody.scrollTop(popupScrollTop);
 
-                $popupHtmlBody.scrollTop(popupScrollTop);
+                    $(articleSelector).addClass('active');
+                    popup.$(articleSelector).addClass('active');
+                }
 
-                $(articleSelector).addClass('active');
-                popup.$(articleSelector).addClass('active');
+                else {
+                    $popupHtmlBody.animate({
+                        scrollTop: popupScrollTop
+                    }, 1000);
+                }
+                
+                $liveFrame.show();
+                resetLiveFramePosition();
             }
-
-            else {
-                $popupHtmlBody.animate({
-                    scrollTop: popupScrollTop
-                }, 1000);
-            }
-            
-            $liveFrame.show();
-            resetLiveFramePosition();
         });
     });
 
